7f82f92dc96a07ffb6f18ca7abc55468c9d31c34
olehhapuk <oleggapuk20@gmail.com>
2023-05-22 21:14:03 +0300

    navbar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,19 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import { Container } from '@mui/material';
 
 import Navbar from './Navbar';
 import Bottombar from './Bottombar';
 
 function Layout() {
   return (
-    <div>
+    <Container maxWidth="xs">
       <Navbar />
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
       <Bottombar />
-    </div>
+    </Container>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,35 @@
+import { Avatar, Typography, Stack, IconButton, Badge } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+
+const NotificationsBadge = styled(Badge)(() => ({
+  '& .MuiBadge-badge': {
+    right: 8,
+    top: 7,
+    minWidth: '10px',
+    minHeight: '10px',
+  },
+}));
+
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <Stack
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      sx={{ paddingY: 3 }}
+    >
+      <Avatar alt="John Doe" variant="rounded">
+        JD
+      </Avatar>
+      <Typography variant="h6">Home</Typography>
+      <IconButton>
+        <NotificationsBadge variant="dot" color="warning">
+          <NotificationsNoneIcon color="action" sx={{ fontSize: 28 }} />
+        </NotificationsBadge>
+      </IconButton>
+    </Stack>
+  );
 }
 
 export default Navbar;
