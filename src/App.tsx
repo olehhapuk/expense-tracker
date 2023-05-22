9631db95c63c48993674410d52a2e38781441c9e
olehhapuk <oleggapuk20@gmail.com>
2023-05-22 20:58:02 +0300

    added icons, fonts, mui css baseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
 
 import { router } from './config/router';
 
 function App() {
   return (
     <>
+      <CssBaseline />
       <RouterProvider router={router} />
     </>
   );
